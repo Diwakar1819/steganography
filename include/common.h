898e515edb569ff47ec7ee6abf8cf43810bf68ff
Diwakar1819 <diwakarravikumar2004@gmail.com>
2026-08-30 15:48:50 +0530

    feat: add common status definitions

---
+++ include/common.h
@@ -0,0 +1,35 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+/*
+ * @brief Represents the result of an operation in the
+ *        steganography application.
+ *
+ * A value of STEG_SUCCESS indicates successful execution.
+ * All other values represent a specific failure condition.
+ */
+typedef enum
+{
+    STEG_SUCCESS = 0,
+
+    /* General errors */
+    STEG_ERR_INVALID_ARGUMENT,
+    STEG_ERR_MEMORY,
+
+    /* File-related errors */
+    STEG_ERR_FILE_OPEN,
+    STEG_ERR_FILE_READ,
+    STEG_ERR_FILE_WRITE,
+
+    /* BMP-related errors */
+    STEG_ERR_INVALID_BMP,
+    STEG_ERR_UNSUPPORTED_BMP,
+    STEG_ERR_INSUFFICIENT_CAPACITY,
+
+    /* Payload-related errors */
+    STEG_ERR_INVALID_PAYLOAD,
+    STEG_ERR_CHECKSUM_MISMATCH
+
+} StegStatus;
+
+#endif /* COMMON_H */
